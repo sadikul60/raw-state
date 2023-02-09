4a9946d63654f983576e1a225fa4dfbe57ab45b5
mdsadikul <mdsadikulislam891@gmail.com>
2023-02-09 19:06:23 +0600

    added counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import RawState from './components/RawState/RawState';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Raw State</h1>
+      <RawState />
     </div>
   );
 }
